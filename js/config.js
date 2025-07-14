d92a87a9d3b57a73acb79e6d25eebed2c93c2c66
Mohd Redzuan <152396475+mRedzuan451@users.noreply.github.com>
2025-07-14 13:09:28 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 
-export const API_URL = 'http://localhost/mancis-cmms/backend';
+export const API_URL = 'http://192.168.141.42/mancis-cmms/backend';
 
 export const state = {
   currentUser: null,
